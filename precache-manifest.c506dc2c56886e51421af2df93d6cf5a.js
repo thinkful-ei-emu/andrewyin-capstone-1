4c355f2b0c769d79f421bcfa6524f9b2f3d097e4
Andrew <andrewyin1994@gmail.com>
2019-08-06 09:53:37 -0700

    Updates

---
+++ precache-manifest.c506dc2c56886e51421af2df93d6cf5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29b9a9263ee9490892a112ca87c6085d",
+    "url": "/thinkful-ei-emu/andrewyin-capstone-1/index.html"
+  },
+  {
+    "revision": "008fffde0666203ad563",
+    "url": "/thinkful-ei-emu/andrewyin-capstone-1/static/css/main.b4c1e790.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/thinkful-ei-emu/andrewyin-capstone-1/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "008fffde0666203ad563",
+    "url": "/thinkful-ei-emu/andrewyin-capstone-1/static/js/main.9ce14fe7.chunk.js"
+  },
+  {
+    "revision": "8322dc088552a46f2611",
+    "url": "/thinkful-ei-emu/andrewyin-capstone-1/static/js/runtime~main.b60ce80e.js"
+  }
+]);
