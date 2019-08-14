346cdc88664f4f16f76d5b0aa8423ee67b7b4503
Andrew <andrewyin1994@gmail.com>
2019-08-14 13:59:54 -0700

    Updates

---
+++ precache-manifest.0b44367e2ce8358a3a362e65c989d897.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d44054a845457fb595ecbff6888bcf54",
+    "url": "/andrewyin-capstone-1-client/index.html"
+  },
+  {
+    "revision": "cdcab5367f1d94b83180",
+    "url": "/andrewyin-capstone-1-client/static/css/main.1672242d.chunk.css"
+  },
+  {
+    "revision": "3d4eb6e4d14bead44438",
+    "url": "/andrewyin-capstone-1-client/static/js/2.8a06cb5b.chunk.js"
+  },
+  {
+    "revision": "cdcab5367f1d94b83180",
+    "url": "/andrewyin-capstone-1-client/static/js/main.40c89baa.chunk.js"
+  },
+  {
+    "revision": "d238e693b09bb5f3b648",
+    "url": "/andrewyin-capstone-1-client/static/js/runtime~main.4ea6bef2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/andrewyin-capstone-1/precache-manifest.d171b47700b339283010fa8668dc9046.js"
+  "/andrewyin-capstone-1-client/precache-manifest.0b44367e2ce8358a3a362e65c989d897.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/andrewyin-capstone-1/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/andrewyin-capstone-1-client/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
