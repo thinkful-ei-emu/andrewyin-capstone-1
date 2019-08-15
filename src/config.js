8743ec49210c0e6e841784b47ebfbe09cf9caa86
Andrew <andrewyin1994@gmail.com>
2019-08-15 13:27:16 -0700

    heroku config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export default {
   API_ENDPOINT: process.env.API_ENDPOINT || 'localhost:8080/api',
   TOKEN_KEY: process.env.TOKEN_KEY || 'totally-secure-key'
