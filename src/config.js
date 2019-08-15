8546be2120a075025b3b7b0011ef848d0969e9eb
Andrew <andrewyin1994@gmail.com>
2019-08-15 13:42:36 -0700

    revert config

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  API_ENDPOINT: process.env.API_ENDPOINT || 'https://intense-wildwood-33210.herokuapp.com/api',
-  TOKEN_KEY: process.env.TOKEN_KEY || '19ea5c82-f931-47ae-8b75-f18c43580b85'
+  API_ENDPOINT: process.env.API_ENDPOINT || 'localhost:8080/api',
+  TOKEN_KEY: process.env.TOKEN_KEY || 'totally-secure-key'
 };
